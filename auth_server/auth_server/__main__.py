f656e8a0104f8936eebcf6d624249a636c5fa242
heiskane <henipe@gmail.com>
2024-05-24 19:46:57 +0300

    docker multi-stage build

---
+++ auth_server/auth_server/__main__.py
@@ -0,0 +1,10 @@
+import uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "auth_server.main:app",
+        host="0.0.0.0",
+        port=8001,
+        log_level="info",
+        reload=True,
+    )
