1fe076d5663d00590f8c731501550a8236aeac60
heiskane <henipe@gmail.com>
2024-05-27 19:40:43 +0300

    use async redis

--- auth_server/auth_server/main.py
+++ auth_server/auth_server/main.py
@@ -11,7 +11,8 @@ from cryptography.hazmat.primitives.asymmetric import padding, rsa
 from fastapi import Cookie, Depends, FastAPI, HTTPException, Response
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from redis.client import Redis
+from redis import asyncio as redis
+from redis.asyncio.client import Redis
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
@@ -72,8 +73,12 @@ async def get_db_session() -> AsyncSession:
         await session.close()
 
 
-def get_redis_client() -> Redis:
-    return redis.Redis(host="redis", port=6379)
+async def get_redis_client() -> Redis:
+    client = redis.Redis(host="redis", port=6379)
+    try:
+        return client
+    finally:
+        await client.aclose()
 
 
 async def require_auth(
@@ -84,7 +89,7 @@ async def require_auth(
     if auth_token is None:
         raise HTTPException(status_code=HTTP_401_UNAUTHORIZED)
 
-    user_id = redis_client.get(f"user:{auth_token}:auth_token")
+    user_id = await redis_client.get(f"user:{auth_token}:auth_token")
 
     if user_id is None:
         raise HTTPException(status_code=HTTP_401_UNAUTHORIZED)
@@ -169,7 +174,7 @@ async def send_challenge(
 
     challenge = secrets.token_bytes(128)
 
-    redis_client.set(
+    await redis_client.set(
         f"user:{user.id}:challenge",
         challenge,
         ex=settings.auth_challenge_ttl_seconds,
@@ -206,18 +211,18 @@ async def auth(
     if user is None:
         raise HTTPException(status_code=HTTP_404_NOT_FOUND)
 
-    challenge = redis_client.get(f"user:{user.id}:challenge")
+    challenge = await redis_client.get(f"user:{user.id}:challenge")
     if b64decode(auth_request.decrypted_challenge) == challenge:
         auth_token = uuid4()
 
-        redis_client.set(
+        await redis_client.set(
             f"user:{auth_token.hex}:auth_token",
             user.id.bytes,
             ex=settings.auth_token_ttl_seconds,
         )
 
         # delete used challenge
-        redis_client.delete(f"user:{user.id}:challenge")
+        await redis_client.delete(f"user:{user.id}:challenge")
 
         response.set_cookie(
             "auth_token",
