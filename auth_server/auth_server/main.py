e75ab2064471b1815c102f2f3f26858560bb8911
heiskane <henipe@gmail.com>
2024-05-27 19:51:26 +0300

    remove unused code

--- auth_server/auth_server/main.py
+++ auth_server/auth_server/main.py
@@ -230,13 +230,3 @@ async def auth(
         return auth_token.hex
 
     raise HTTPException(status_code=HTTP_401_UNAUTHORIZED)
-
-
-if __name__ == "__main__":
-    uvicorn.run(
-        "main:app",
-        host="0.0.0.0",
-        port=8001,
-        log_level="info",
-        reload=True,
-    )
